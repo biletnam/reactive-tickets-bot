a06cb730509a95e30a5bbd332e3b2fb2502903c6
bsnisar <bogdan.sns@gmail.com>
2016-09-30 08:46:59 +0300

    Token function implementation

--- src/main/scala/org/tickets/bot/uz/TokenFlow.scala
+++ src/main/scala/org/tickets/bot/uz/TokenFlow.scala
@@ -2,40 +2,63 @@ package org.tickets.bot.uz
 
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.client.RequestBuilding
-import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
 import akka.stream.scaladsl.Flow
-import akka.util.ByteString
 import org.tickets.misc.HttpSupport
 
-import scala.util.{Failure, Success}
+import scala.concurrent.Future
 
-/**
-  * Created by bsnisar on 29.09.16.
-  */
+class TokenFlow {
+  import akka.http.scaladsl.util.FastFuture._
+
+  /**
+    * Load token from root page.
+    * @return token
+    */
+  def loadToken: Future[String] =
+    rootPage.fast.flatMap(extractPageBody).map(extractToken)
+
+
+  /**
+    * Fetch root page content.
+    * @return async http response
+    */
+  private def rootPage: Future[HttpResponse] =
+    Http().singleRequest(RequestBuilding.Get(TokenFlow.UzRootPage))
+
+
+  /**
+    * Extract token from page body.
+    * @param pageContent page body as string
+    * @return fresh token
+    */
+  private def extractToken(pageContent: String): String = UzToken(pageContent)
+
+
+  /**
+    * Extract page body as string
+    * @param response http response
+    * @return page body
+    */
+  private def extractPageBody(response: HttpResponse): Future[String] = {
+    val um: FromEntityUnmarshaller[String] =
+      Unmarshaller.byteStringUnmarshaller
+        .mapWithCharset { (data, charset) =>
+          data.decodeString(charset.nioCharset.name)
+        }
+
+    um(response.entity)
+  }
+
+}
+
 object TokenFlow {
   val UzRootPage = "/"
 
-  def getToken() = {
-    Http().singleRequest(RequestBuilding.Get(UzRootPage)) onComplete {
-      case Success(response) =>
-        Unmarshal(response).to[ByteString]
-
-
-      case Failure(error) => error.printStackTrace()
-    }
-
-  }
-
   def foo() = {
-
-
-
-
     Flow.apply[HttpSupport.Request].mapAsyncUnordered[HttpSupport.Request](100) { req =>
-
       ???
     }
-
-
   }
 }
--- src/main/scala/org/tickets/bot/uz/UzToken.scala
+++ src/main/scala/org/tickets/bot/uz/UzToken.scala
@@ -8,24 +8,22 @@ import org.tickets.api.token.JJEncoder
   * Extract token from page content.
   * @author bsnisar
   */
-class UzToken extends (String => String) {
+private object UzToken {
+  val EncodedDataPattern = Pattern.compile("\\$\\$_=.*~\\[\\];.*\"\"\\)\\(\\)\\)\\(\\);")
+  val TokenPattern = Pattern.compile("[0-9a-f]{32}")
 
-  override def apply(content: String): String = {
+
+  def apply(content: String): String = {
     val matcher = UzToken.EncodedDataPattern.matcher(content)
-    require(matcher.find())
+    require(matcher.find(), "Encoded block not found")
 
     val encodedTokenData: String = content.substring(matcher.start, matcher.end)
     val decodedTokenData: String = new JJEncoder().decode(encodedTokenData)
     val tokenMatcher = UzToken.TokenPattern.matcher(decodedTokenData)
-    require(tokenMatcher.find())
+    require(tokenMatcher.find(), "Token not found")
 
     val token: String = decodedTokenData.substring(matcher.start, matcher.end)
     token
   }
-}
-
-private object UzToken {
-  val EncodedDataPattern = Pattern.compile("\\$\\$_=.*~\\[\\];.*\"\"\\)\\(\\)\\)\\(\\);")
-  val TokenPattern = Pattern.compile("[0-9a-f]{32}")
 }
 
