45c9dc89a199d66c5766e3ab33e3cca65d649f17
bsnisar <bogdan.sns@gmail.com>
2017-01-04 09:42:34 +0200

    search by criteria - next changes

--- src/main/scala/com/github/bsnisar/tickets/sub/DbSubscription.scala
+++ src/main/scala/com/github/bsnisar/tickets/sub/DbSubscription.scala
@@ -1,20 +1,21 @@
 package com.github.bsnisar.tickets.sub
 
-import java.sql.Clob
-
 import com.google.common.hash.Hashing
 import com.jcabi.xml.XML
 
-class DbSubscription extends Subscriptions {
-  override def add(client: String, query: XML): Unit = {
-    val xmlQuery = query.toString
-    val hashCode = Hashing.md5().hashBytes(xmlQuery.getBytes).toString
+import slick.driver.H2Driver.api._
 
+class DbSubscription extends Subscriptions {
+
+  override def add(client: String, query: XML): Unit = {
+    val queryStr = query.toString
+    val hash = Hashing.md5().hashBytes(queryStr.getBytes).toString
+
+    DbSubscription.insert(hash, queryStr, client)
   }
 }
 
-private object DB {
-  import slick.driver.H2Driver.api._
+private object DbSubscription {
 
   class Req(tag: Tag) extends Table[(String, String)](tag, "REQ"){
     def hashID = column[String]("HASH_ID", O.PrimaryKey)
@@ -24,19 +25,24 @@ private object DB {
 
   val requests = TableQuery[Req]
 
-  class Client(tag: Tag) extends Table[(Long, String)](tag, "CLIENT") {
-    def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
-    def hash = column[String]("HASH")
-    def req = foreignKey("CLIENT_REQ_FK", hash, requests)(_.hashID, onUpdate = ForeignKeyAction.Restrict)
-    def * = (id, hash)
+  class Client(tag: Tag) extends Table[(String, String)](tag, "CLIENT") {
+    def clientID = column[String]("CLIENT_ID", O.PrimaryKey, O.AutoInc)
+    def reqID = column[String]("REQ_ID")
+    def req = foreignKey("CLIENT_REQ_FK", reqID, requests)(_.hashID, onUpdate = ForeignKeyAction.Restrict)
+    def * = (clientID, reqID)
   }
 
   val clients = TableQuery[Client]
 
 
-  def insertReqIfNotExists(hash: String, xml: String) = requests.forceInsertQuery {
+  def addReqIfNotExists(hash: String, xml: String): DBIOAction[_,NoStream,_] = requests.forceInsertQuery {
     val exists = (for (req <- requests if req.hashID === hash) yield req).exists
     val insert = (hash, xml)
     for (u <- Query(insert) if !exists) yield u
   }
+
+  def insert(hash: String, xml: String, clientId: String): DBIOAction[Unit,NoStream,_] = DBIO.seq(
+    addReqIfNotExists(hash, xml),
+    clients += (clientId, hash)
+  )
 }
